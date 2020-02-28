a546bc69e299ec2c4c00ad13356594490a2e26b4
Reid Weber <reid@meshstudio.io>
2020-02-28 11:31:23 -0800

    Added Network Call with URL

--- src/services/NetworkingService.ts
+++ src/services/NetworkingService.ts
@@ -1,15 +1,29 @@
 import Axios, { AxiosResponse } from 'axios'
+import AppId from '../appId/AppId'
 
-const baseURL: string = ''
+const baseURL: string = 'https://api.openweathermap.org/data/2.5/weather'
+
+interface WeatherParams {
+  appid: String
+  q: String
+}
 
 export default class NetworkService {
 
   /**
    * Fetch and return the weather data from the given endpoint.
    */
-  public static fetchWeatherData = (): Promise<AxiosResponse> => {
+  public static fetchWeatherData = (): Promise<any> => {
     const url: string = `${baseURL}`
-    return Axios.get(url)
+    const params: WeatherParams = {
+      appid: AppId,
+      q: 'London,uk'
+    }
+    return Axios.get(url, { params: params }).then((response: AxiosResponse) => {
+      return response
+    }).catch((error: Error) => {
+      return
+    })
   }
 
 }
--- src/views/WeatherView.tsx
+++ src/views/WeatherView.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import NetworkingService from '../services/NetworkingService'
 import {
   View,
   StyleSheet
 } from 'react-native'
 
 export default class WeatherView extends React.Component<any, any> {
+
+  async componentDidMount() {
+    NetworkingService.fetchWeatherData()
+  }
 
   public render() {
     return <View style={styles.flatListContainer} />
