c69af57fa1e901e0178897c8a31e54b35d8933fe
Reid Weber <reid@meshstudio.io>
2020-02-28 12:52:43 -0800

    Added Weather Interfaces

--- src/services/NetworkingService.ts
+++ src/services/NetworkingService.ts
@@ -1,11 +1,15 @@
 import Axios, { AxiosResponse } from 'axios'
 import AppId from '../appId/AppId'
 
+import {
+  WeatherData
+} from '../Interfaces/WeatherData'
+
 const baseURL: string = 'https://api.openweathermap.org/data/2.5/weather'
 
 interface WeatherParams {
-  appid: String
-  q: String
+  appid: string
+  q: string
 }
 
 export default class NetworkService {
@@ -13,16 +17,14 @@ export default class NetworkService {
   /**
    * Fetch and return the weather data from the given endpoint.
    */
-  public static fetchWeatherData = (): Promise<any> => {
+  public static fetchWeatherData = (): Promise<WeatherData> => {
     const url: string = `${baseURL}`
     const params: WeatherParams = {
       appid: AppId,
       q: 'London,uk'
     }
     return Axios.get(url, { params: params }).then((response: AxiosResponse) => {
-      return response
-    }).catch((error: Error) => {
-      return
+      return response.data
     })
   }
 
